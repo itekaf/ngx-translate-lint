090622b1d8c21fc76ce16c7ded932283e50cb2c9
Raman Lauryniuk <raman_lauryniuk@epam.com>
2019-11-05 19:06:23 +0300

    feat: base rule for i18n

---
+++ src/rules/angular/templates/basicTemplateAstVisitor.ts
@@ -0,0 +1,188 @@
+import * as ast from '@angular/compiler';
+import * as ts from 'typescript';
+import * as Lint from 'tslint';
+import * as e from '@angular/compiler/src/expression_parser/ast';
+
+import { ExpTypes } from '../expressionTypes';
+import { ComponentMetadata } from '../metadata';
+import { RecursiveAngularExpressionVisitor } from './recursiveAngularExpressionVisitor';
+import { SourceMappingVisitor } from '../sourceMappingVisitor';
+
+const getExpressionDisplacement = (binding: ast.TemplateAst) => {
+    let attrLen = 0;
+    let valLen = 0;
+    let totalLength = 0;
+    let result = 0;
+    if (
+        binding instanceof ast.BoundEventAst ||
+        binding instanceof ast.BoundElementPropertyAst ||
+        binding instanceof ast.BoundDirectivePropertyAst
+    ) {
+        // subBindingLen is for [class.foo], [style.foo], the length of
+        // the binding type. For event it is 0. (+1 because of the ".")
+        let subBindingLen = 0;
+        if (binding instanceof ast.BoundElementPropertyAst) {
+            // The length of the binding type
+            switch (binding.type) {
+                case ast.PropertyBindingType.Animation:
+                    subBindingLen = 'animate'.length + 1;
+                    break;
+                case ast.PropertyBindingType.Attribute:
+                    subBindingLen = 'attr'.length + 1;
+                    break;
+                case ast.PropertyBindingType.Class:
+                    subBindingLen = 'class'.length + 1;
+                    break;
+                case ast.PropertyBindingType.Style:
+                    subBindingLen = 'style'.length + 1;
+                    break;
+            }
+        }
+        // For [class.foo]=":
+        // - Add the name of the binding (binding.name.length).
+        // - 4 characters because of []=" (we already have the "." from above).
+        // - subBindingLen is from above and applies only for elements.
+        if (!(binding instanceof ast.BoundDirectivePropertyAst)) {
+            attrLen = binding.name.length + 4 + subBindingLen;
+        }
+
+        // Total length of the attribute value
+        if (binding instanceof ast.BoundEventAst) {
+            valLen = binding.handler.span.end;
+        } else if (
+            binding instanceof ast.BoundDirectivePropertyAst &&
+            (binding.templateName === 'ngForOf' || binding.templateName === 'ngIf')
+        ) {
+            // Handling everything except [ngForOf] differently
+            // [ngForOf] and [ngIf] require different logic because it gets desugered
+            result = binding.sourceSpan.start.file.content.lastIndexOf((binding.value as any).source);
+
+            if (binding.templateName === 'ngIf') {
+                if ((binding.value as any).ast.span.start > 0)
+                    result = binding.sourceSpan.start.file.content.lastIndexOf((binding.value as any).source) + (binding.value as any).ast.span.start;
+            }
+        } else {
+            valLen = binding.value.span.end;
+        }
+
+        // Handling everything except [ngForOf]
+        if (!(binding instanceof ast.BoundDirectivePropertyAst) || (binding.templateName !== 'ngForOf' && binding.templateName !== 'ngIf')) {
+            // Total length of the entire part of the template AST corresponding to this AST.
+            totalLength = binding.sourceSpan.end.offset - binding.sourceSpan.start.offset;
+            // The whitespace are possible only in:
+            // `[foo.bar]          =         "...."`,
+            // and they are everything except the attrLen and the valLen (-1 because of the close quote).
+            let whitespace = totalLength - (attrLen + valLen) - 1;
+            // The resulted displacement is the length of the attribute + the whitespaces which
+            // can be located ONLY before the value (the binding).
+            result = whitespace + attrLen + binding.sourceSpan.start.offset;
+        }
+    } else if (binding instanceof ast.BoundTextAst) {
+        result = binding.sourceSpan.start.offset;
+    }
+    return result;
+};
+
+export interface RecursiveAngularExpressionVisitorCtr {
+    new (sourceFile: ts.SourceFile, options: Lint.IOptions, context: ComponentMetadata, basePosition: number);
+}
+
+export interface TemplateAstVisitorCtr {
+    new (
+        sourceFile: ts.SourceFile,
+        options: Lint.IOptions,
+        context: ComponentMetadata,
+        templateStart: number,
+        expressionVisitorCtrl: RecursiveAngularExpressionVisitorCtr
+    );
+}
+
+export class BasicTemplateAstVisitor extends SourceMappingVisitor implements ast.TemplateAstVisitor {
+    private _variables = {};
+
+    constructor(
+        sourceFile: ts.SourceFile,
+        private _originalOptions: Lint.IOptions,
+        protected context: ComponentMetadata,
+        protected templateStart: number,
+        private expressionVisitorCtrl: RecursiveAngularExpressionVisitorCtr = RecursiveAngularExpressionVisitor
+    ) {
+        super(sourceFile, _originalOptions, context.template!.template, templateStart);
+    }
+
+    visit?(node: ast.TemplateAst, context: any): any {
+        node.visit(this, context);
+    }
+
+    visitNgContent(ast: ast.NgContentAst, context: any): any {}
+
+    visitEmbeddedTemplate(ast: ast.EmbeddedTemplateAst, context: any): any {
+        ast.directives.forEach(d => this.visit!(d, context));
+        ast.variables.forEach(v => this.visit!(v, context));
+        ast.children.forEach(e => this.visit!(e, context));
+        ast.outputs.forEach(o => this.visit!(o, context));
+        ast.attrs.forEach(a => this.visit!(a, context));
+        ast.references.forEach(r => this.visit!(r, context));
+    }
+
+    visitElement(element: ast.ElementAst, context: any): any {
+        element.references.forEach(r => this.visit!(r, context));
+        element.inputs.forEach(i => this.visit!(i, context));
+        element.outputs.forEach(o => this.visit!(o, context));
+        element.attrs.forEach(a => this.visit!(a, context));
+        element.children.forEach(e => this.visit!(e, context));
+        element.directives.forEach(d => this.visit!(d, context));
+    }
+
+    visitReference(ast: ast.ReferenceAst, context: any): any {}
+
+    visitVariable(ast: ast.VariableAst, context: any): any {
+        this._variables[ast.name] = true;
+    }
+
+    visitEvent(ast: ast.BoundEventAst, context: any): any {
+        this._variables['$event'] = true;
+        this.visitNgTemplateAST(ast.handler, this.templateStart + getExpressionDisplacement(ast));
+        this._variables['$event'] = false;
+    }
+
+    visitElementProperty(prop: ast.BoundElementPropertyAst, context: any): any {
+        const ast = (prop.value as e.ASTWithSource).ast;
+        (ast as any).interpolateExpression = (prop.value as any).source;
+        this.visitNgTemplateAST(prop.value, this.templateStart + getExpressionDisplacement(prop), prop);
+    }
+
+    visitAttr(ast: ast.AttrAst, context: any): any {}
+
+    visitBoundText(text: ast.BoundTextAst, context: any): any {
+        if (ExpTypes.ASTWithSource(text.value)) {
+            // Note that will not be reliable for different interpolation symbols
+            const ast = (text.value as e.ASTWithSource).ast;
+            (ast as any).interpolateExpression = (text.value as any).source;
+            this.visitNgTemplateAST(ast, this.templateStart + getExpressionDisplacement(text));
+        }
+    }
+
+    visitText(text: ast.TextAst, context: any): any {}
+
+    visitDirective(ast: ast.DirectiveAst, context: any): any {
+        ast.inputs.forEach(o => this.visit!(o, context));
+        ast.hostProperties.forEach(p => this.visit!(p, context));
+        ast.hostEvents.forEach(e => this.visit!(e, context));
+    }
+
+    visitDirectiveProperty(prop: ast.BoundDirectivePropertyAst, context: any): any {
+        if (ExpTypes.ASTWithSource(prop.value)) {
+            this.visitNgTemplateAST(prop.value, this.templateStart + getExpressionDisplacement(prop), prop);
+        }
+    }
+
+    protected visitNgTemplateAST(ast: e.AST, templateStart: number, prop?: any) {
+        const templateVisitor = new this.expressionVisitorCtrl(this.getSourceFile(), this._originalOptions, this.context, templateStart);
+        templateVisitor.preDefinedVariables = this._variables;
+        templateVisitor.parentAST = prop;
+        templateVisitor.visit(ast);
+        // tslint:disable-next-line:deprecation
+        templateVisitor.getFailures().forEach(f => this.addFailure(f));
+    }
+}
---
+++ src/rules/template-i18n-rule.ts
@@ -0,0 +1,244 @@
+import { ASTWithSource, AttrAst, BoundTextAst, ElementAst, Interpolation, TemplateAst, TextAst } from '@angular/compiler';
+import { IRuleMetadata, RuleFailure } from 'tslint';
+import { AbstractRule } from 'tslint/lib/rules';
+import { arrayify, dedent } from 'tslint/lib/utils';
+import { SourceFile } from 'typescript';
+import { NgWalker, NgWalkerConfig } from './angular/ngWalker';
+import { BasicTemplateAstVisitor } from './angular/templates/basicTemplateAstVisitor';
+import { isNotNullOrUndefined } from './util/isNotNullOrUndefined';
+
+const OPTION_CHECK_ID: string = 'check-id';
+const OPTION_CHECK_TEXT: string = 'check-text';
+
+type CheckOption = typeof OPTION_CHECK_ID | typeof OPTION_CHECK_TEXT;
+
+interface IConfigurableVisitor {
+    getCheckOption(): CheckOption;
+}
+
+interface IFailureParameters {
+    readonly ast: AttrAst | BoundTextAst | TextAst;
+    readonly context: BasicTemplateAstVisitor;
+    readonly message: typeof Rule.FAILURE_STRING_ATTR | typeof Rule.FAILURE_STRING_TEXT;
+}
+
+const generateFailure: Function = (failureParameters: IFailureParameters): void => {
+    const {
+        sourceSpan: {
+            end: { offset: endOffset },
+            start: { offset: startOffset }
+        }
+    } = failureParameters.ast;
+
+    failureParameters.context.addFailureFromStartToEnd(startOffset, endOffset, failureParameters.message);
+};
+
+export class Rule extends AbstractRule {
+    public static readonly metadata: IRuleMetadata = {
+        description: 'Ensures following best practices for i18n.',
+        optionExamples: [[true, OPTION_CHECK_ID], [true, OPTION_CHECK_TEXT], [true, OPTION_CHECK_ID, OPTION_CHECK_TEXT]],
+        options: {
+            items: {
+                enum: [OPTION_CHECK_ID, OPTION_CHECK_TEXT],
+                type: 'string'
+            },
+            maxLength: 2,
+            minLength: 1,
+            type: 'array'
+        },
+        optionsDescription: dedent`
+      One (or both) of the following arguments must be provided:
+      * \`${OPTION_CHECK_ID}\` Makes sure i18n attributes have ID specified
+      * \`${OPTION_CHECK_TEXT}\` Makes sure there are no elements with text content but no i18n attribute
+    `,
+        rationale: 'Makes the code more maintainable in i18n sense.',
+        ruleName: 'template-i18n',
+        type: 'maintainability',
+        typescriptOnly: true
+    };
+
+    public static readonly FAILURE_STRING_ATTR: string = 'Missing custom message identifier. For more information visit https://angular.io/guide/i18n';
+    public static readonly FAILURE_STRING_TEXT: string = 'Each element containing text node should have an i18n attribute';
+
+    public apply(sourceFile: SourceFile): RuleFailure[] {
+        const walkerConfig: NgWalkerConfig = { templateVisitorCtrl: TemplateVisitorCtrl };
+        const walker = new NgWalker(sourceFile, this.getOptions(), walkerConfig);
+
+        return this.applyWithWalker(walker);
+    }
+
+    public isEnabled(): boolean {
+        const {
+            metadata: {
+                options: {
+                    items: { enum: enumItems },
+                    maxLength,
+                    minLength
+                }
+            }
+        } = Rule;
+
+        // const argumentsLength: number = this.ruleArguments.length;
+        const { length: argumentsLength } = this.ruleArguments;
+        const optionArgument: any[] = arrayify(this.ruleArguments).filter(isNotNullOrUndefined);
+        const argumentsLengthInRange: boolean = argumentsLength >= minLength && argumentsLength <= maxLength;
+        const isOptionArgumentValid = optionArgument.length > 0 && optionArgument.every(argument => enumItems.indexOf(argument) !== -1);
+
+        return super.isEnabled() && argumentsLengthInRange && isOptionArgumentValid;
+    }
+}
+
+class TemplateVisitorAttrCtrl extends BasicTemplateAstVisitor implements IConfigurableVisitor {
+    public getCheckOption(): CheckOption {
+        return OPTION_CHECK_ID;
+    }
+
+    public visitAttr(ast: AttrAst, context: BasicTemplateAstVisitor): any {
+        this.validateAttr(ast, context);
+        super.visitAttr(ast, context);
+    }
+
+    private validateAttr(ast: AttrAst, context: BasicTemplateAstVisitor): void {
+        if (ast.name !== 'i18n') return;
+
+        const parts = ast.value.split('@@');
+
+        if (parts.length > 1 && parts[1].length !== 0) return;
+
+        generateFailure({ ast, context, message: Rule.FAILURE_STRING_ATTR });
+    }
+}
+
+class TemplateVisitorTextCtrl extends BasicTemplateAstVisitor implements IConfigurableVisitor {
+    private hasI18n: boolean = false;
+    private readonly nestedElements: string[] = [];
+    private readonly visited: Set<BoundTextAst | TextAst> = new Set<BoundTextAst | TextAst>();
+
+    public getCheckOption(): CheckOption {
+        return OPTION_CHECK_TEXT;
+    }
+
+    public visitBoundText(text: BoundTextAst, context: BasicTemplateAstVisitor): any {
+        this.validateBoundText(text, context);
+        super.visitBoundText(text, context);
+    }
+
+    public visitElement(element: ElementAst, context: BasicTemplateAstVisitor): any {
+        const originalI18n: boolean = this.hasI18n;
+        this.hasI18n = originalI18n || element.attrs.some(e => e.name === 'i18n');
+        this.nestedElements.push(element.name);
+        super.visitElement(element, context);
+        this.nestedElements.pop();
+        this.hasI18n = originalI18n;
+        super.visitElement(element, context);
+    }
+
+    public visitText(text: TextAst, context: BasicTemplateAstVisitor): any {
+        this.validateText(text, context);
+        super.visitText(text, context);
+    }
+
+    private validateBoundText(text: BoundTextAst, context: BasicTemplateAstVisitor): void {
+        if (this.visited.has(text)) return;
+
+        this.visited.add(text);
+
+        const { value } = text;
+
+        if (!(value instanceof ASTWithSource) || !(value.ast instanceof Interpolation) || this.hasI18n) {
+            return;
+        }
+
+        const isTextEmpty = !value.ast.strings.some(s => /\w+/.test(s));
+
+        if (isTextEmpty) return;
+
+        generateFailure({ ast: text, context, message: Rule.FAILURE_STRING_TEXT });
+    }
+
+    private validateText(text: TextAst, context: BasicTemplateAstVisitor): void {
+        if (this.visited.has(text)) return;
+
+        this.visited.add(text);
+        const isTextEmpty = text.value.trim().length === 0;
+
+        if (isTextEmpty || (this.hasI18n && this.nestedElements.length > 0)) return;
+
+        generateFailure({ ast: text, context, message: Rule.FAILURE_STRING_TEXT });
+    }
+}
+
+class TemplateVisitorCtrl extends BasicTemplateAstVisitor {
+    private readonly visitors: ReadonlyArray<BasicTemplateAstVisitor & IConfigurableVisitor> = [
+        new TemplateVisitorAttrCtrl(this.getSourceFile(), this.getOptions(), this.context, this.templateStart),
+        new TemplateVisitorTextCtrl(this.getSourceFile(), this.getOptions(), this.context, this.templateStart)
+    ];
+
+    public visit(node: TemplateAst, context: BasicTemplateAstVisitor): any {
+        super.visit!(node, context);
+    }
+
+    public visitAttr(ast: AttrAst, context: BasicTemplateAstVisitor): any {
+        this.validateAttr(ast);
+        super.visitAttr(ast, context);
+    }
+
+    public visitBoundText(text: BoundTextAst, context: any): any {
+        this.validateBoundText(text);
+        super.visitBoundText(text, context);
+    }
+
+    public visitElement(element: ElementAst, context: BasicTemplateAstVisitor): any {
+        this.validateElement(element);
+        super.visitElement(element, context);
+    }
+
+    public visitText(text: TextAst, context: BasicTemplateAstVisitor): any {
+        this.validateText(text);
+        super.visitText(text, context);
+    }
+
+    private validateAttr(ast: AttrAst): void {
+        const options = this.getOptions();
+        this.visitors
+            .filter(v => options.indexOf(v.getCheckOption()) !== -1)
+            .map(v => v.visitAttr(ast, this))
+            .filter(isNotNullOrUndefined)
+            .forEach(f =>
+                this.addFailureFromStartToEnd(f.getStartPosition().getPosition(), f.getEndPosition().getPosition(), f.getFailure(), f.getFix())
+            );
+    }
+
+    private validateBoundText(text: BoundTextAst): void {
+        const options = this.getOptions();
+        this.visitors
+            .filter(v => options.indexOf(v.getCheckOption()) !== -1)
+            .map(v => v.visitBoundText(text, this))
+            .filter(isNotNullOrUndefined)
+            .forEach(f =>
+                this.addFailureFromStartToEnd(f.getStartPosition().getPosition(), f.getEndPosition().getPosition(), f.getFailure(), f.getFix())
+            );
+    }
+
+    private validateElement(element: ElementAst): void {
+        const options = this.getOptions();
+        this.visitors
+            .filter(v => options.indexOf(v.getCheckOption()) !== -1)
+            .map(v => v.visitElement(element, this))
+            .filter(isNotNullOrUndefined)
+            .forEach(f =>
+                this.addFailureFromStartToEnd(f.getStartPosition().getPosition(), f.getEndPosition().getPosition(), f.getFailure(), f.getFix())
+            );
+    }
+
+    private validateText(text: TextAst): void {
+        const options = this.getOptions();
+        this.visitors
+            .filter(v => options.indexOf(v.getCheckOption()) !== -1)
+            .map(v => v.visitText(text, this))
+            .filter(isNotNullOrUndefined)
+            .forEach(f =>
+                this.addFailureFromStartToEnd(f.getStartPosition().getPosition(), f.getEndPosition().getPosition(), f.getFailure(), f.getFix())
+            );
+    }
+}
