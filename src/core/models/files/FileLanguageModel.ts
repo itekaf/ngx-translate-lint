6488dfaa416f72c4472ee2a5ba2fb25c77e4cde0
svoboda-rabstvo <romanrostislavovich@gmail.com>
2020-12-11 12:58:04 +0300

    fix: linter doesn't exist and return any result

--- src/core/models/files/FileLanguageModel.ts
+++ src/core/models/files/FileLanguageModel.ts
@@ -2,6 +2,7 @@ import { get } from 'lodash';
 
 import { KeyModel } from '../KeyModel';
 import { FileModel } from './FileModel';
+import { Key } from 'readline';
 
 class FileLanguageModel extends FileModel {
     constructor(
@@ -17,11 +18,11 @@ class FileLanguageModel extends FileModel {
         this.files =  this.getNormalizeFiles();
         this.keys = this.parseKeys((fileData: string, filePath: string): KeyModel[] => {
             try {
-                const fileKeysNames: string[] = this.getLanguageKeys(JSON.parse(fileData));
+                const fileKeysNames: KeyModel[] = this.getLanguageKeys(JSON.parse(fileData));
                 return !fileKeysNames ? [] : fileKeysNames
                     .filter(x => !!x)
-                    .map((key: string) => {
-                        return new KeyModel(key, [], [filePath]);
+                    .map((key) => {
+                        return new KeyModel(key.name, [], [filePath], key.value);
                     });
             } catch (e) {
                 const errorMessage: string = `Can't parse JSON file: ${filePath}`;
@@ -32,23 +33,29 @@ class FileLanguageModel extends FileModel {
        return this;
     }
 
-    private getLanguageKeys(obj: object, cat: string | null = null, tKeys: string[] = []): string[] {
+    private getLanguageKeys(obj: object, cat: string | null = null, tKeys: string[] = [], keys: KeyModel[] = []): KeyModel[] {
         const currentKeys: string[] = [ ...tKeys ];
         const objectKeys: string[] = Object.keys(obj);
-
+        const correctKeyModel: KeyModel[] = keys;
         objectKeys.forEach((key: string) => {
             const keyValue: object = get(obj, key);
             const isObject: boolean = typeof keyValue === 'object';
             const currentKey: string =  cat === null ? key : cat.concat('.', key);
 
             if (isObject) {
-                const childKeys: string[] = this.getLanguageKeys(keyValue, currentKey, tKeys);
-                currentKeys.push(...childKeys);
+                const childKeys: KeyModel[] = this.getLanguageKeys(keyValue, currentKey, tKeys, correctKeyModel);
+                currentKeys.push(...childKeys.map((x) => x.name));
             } else {
                 currentKeys.push(currentKey);
+                correctKeyModel.push({
+                    name: currentKey,
+                    value: keyValue.toString(),
+                    views: [],
+                    languages: []
+                });
             }
         });
-        return currentKeys;
+        return correctKeyModel;
     }
 }
 
