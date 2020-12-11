95681838dd452cf79f1a9ed727af16e829d63418
svoboda-rabstvo <romanrostislavovich@gmail.com>
2020-12-11 12:59:23 +0300

    Revert "fix: linter doesn't exist and return any result"
    This reverts commit 6488dfaa416f72c4472ee2a5ba2fb25c77e4cde0.

--- src/core/models/KeyModel.ts
+++ src/core/models/KeyModel.ts
@@ -1,12 +1,11 @@
 class KeyModel {
     public name: string;
-    public value: string = '';
+
     public views: string[] = [];
     public languages: string[] = [];
 
-    constructor(name: string, views: string[] = [], languages: string[] = [], value: string = '') {
+    constructor(name: string, views: string[] = [], languages: string[] = []) {
         this.name = name;
-        this.value = value;
         this.views = views;
         this.languages = languages;
     }
--- src/core/models/files/FileLanguageModel.ts
+++ src/core/models/files/FileLanguageModel.ts
@@ -2,7 +2,6 @@ import { get } from 'lodash';
 
 import { KeyModel } from '../KeyModel';
 import { FileModel } from './FileModel';
-import { Key } from 'readline';
 
 class FileLanguageModel extends FileModel {
     constructor(
@@ -18,11 +17,11 @@ class FileLanguageModel extends FileModel {
         this.files =  this.getNormalizeFiles();
         this.keys = this.parseKeys((fileData: string, filePath: string): KeyModel[] => {
             try {
-                const fileKeysNames: KeyModel[] = this.getLanguageKeys(JSON.parse(fileData));
+                const fileKeysNames: string[] = this.getLanguageKeys(JSON.parse(fileData));
                 return !fileKeysNames ? [] : fileKeysNames
                     .filter(x => !!x)
-                    .map((key) => {
-                        return new KeyModel(key.name, [], [filePath], key.value);
+                    .map((key: string) => {
+                        return new KeyModel(key, [], [filePath]);
                     });
             } catch (e) {
                 const errorMessage: string = `Can't parse JSON file: ${filePath}`;
@@ -33,29 +32,23 @@ class FileLanguageModel extends FileModel {
        return this;
     }
 
-    private getLanguageKeys(obj: object, cat: string | null = null, tKeys: string[] = [], keys: KeyModel[] = []): KeyModel[] {
+    private getLanguageKeys(obj: object, cat: string | null = null, tKeys: string[] = []): string[] {
         const currentKeys: string[] = [ ...tKeys ];
         const objectKeys: string[] = Object.keys(obj);
-        const correctKeyModel: KeyModel[] = keys;
+
         objectKeys.forEach((key: string) => {
             const keyValue: object = get(obj, key);
             const isObject: boolean = typeof keyValue === 'object';
             const currentKey: string =  cat === null ? key : cat.concat('.', key);
 
             if (isObject) {
-                const childKeys: KeyModel[] = this.getLanguageKeys(keyValue, currentKey, tKeys, correctKeyModel);
-                currentKeys.push(...childKeys.map((x) => x.name));
+                const childKeys: string[] = this.getLanguageKeys(keyValue, currentKey, tKeys);
+                currentKeys.push(...childKeys);
             } else {
                 currentKeys.push(currentKey);
-                correctKeyModel.push({
-                    name: currentKey,
-                    value: keyValue.toString(),
-                    views: [],
-                    languages: []
-                });
             }
         });
-        return correctKeyModel;
+        return currentKeys;
     }
 }
 
